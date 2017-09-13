64faf971e309c4d323d3767e81680df42a7798ad
wshaman <serge@shtriling.com>
2017-09-13 22:51:47 +0300

    [+] Seeding users

--- commands/SeedController.php
+++ commands/SeedController.php
@@ -45,12 +45,24 @@ class SeedController extends Controller
 
     public function actionUsers()
     {
-        $user = new User();
-        $user->skip_before_save = true;
-        $user->email = 'admin@admin.com';
-        $user->setPassword('admin');
-        $user->username = 'admin';
-        $user->status = $user::STATUS_ACTIVE;
-        $user->save(false);
+        $now = time();
+        $users = [
+            ['auth_key' => '2d4-7O2iHHxAZc4kIz7AlIPNvp5bnbiY', 'password_hash'=>'$2y$13$X2yxIhAuvz54ZKwGHZw58eqI22UJAChgaE01NUNgH6.rKX1GaRR0a', 'email'=>'admin@admin.com', 'role_name'=>'admin'],
+            ['auth_key' => '4aJeVTq5uWnxi3Rx5D7TdZjcXeyx5oNZ', 'password_hash'=>'$2y$13$r6v5rPYWhpbkirUJTKv7tOeEyJHAQVDvR2fXkrgDeCYX.YriYCOQO', 'email'=>'manager@manager.com', 'role_name'=>'manager'],
+            ['auth_key' => 'E-yEOphMKLcilU13D38WGA8uhgwH-J-r', 'password_hash'=>'$2y$13$FftLuN8r8IMVKeaIA5JZqO95g8/cY37XxS7tOJkQKpyAFrFXpdbvq', 'email'=>'reader@reader.com', 'role_name'=>'reader']
+        ];
+        $data = [
+            'status' => 1,
+            'created_at' => $now,
+            'updated_at' => $now,
+            'confirmed_on' => $now,
+            'last_login' => $now,
+            'last_login_ip' => '127.0.0.1',
+            'register_ip' => '127.0.0.1',
+            ];
+        foreach ($users as $user) {
+            $rows = array_merge($user, $data);
+            \Yii::$app->db->createCommand()->insert('{{%user}}', $rows)->execute();
+        }
     }
 }
--- run.sh
+++ run.sh
@@ -3,12 +3,13 @@ composer update
 DBCONFIG="./config/db.php"
 if [ -a $DBCONFIG ]
 then
-    echo "$DBCONFIG file exists, skipping"
+    echo "$DBCONFIG file exists, can proceed"
+    php yii migrate/up --migrationPath=@vendor/nkostadinov/yii2-user/migrations
+    php yii rbac/init
+    php yii migrate
+    php yii seed 100
+    php yii seed/users
 else
     cp ./config/db.example.php $DBCONFIG
-    echo "Don't forget to update $DBCONFIG"
+    echo "Don't forget to update $DBCONFIG and run this script again"
 fi
-php yii migrate/up --migrationPath=@vendor/nkostadinov/yii2-user/migrations
-php yii rbac/init
-php yii migrate
-php yii seed 100
